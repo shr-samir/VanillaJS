78bc71bf62f0dc0a1123e0c89399a5a120cdde7c
Samir Shrestha <linkwithsamir@gmail.com>
2024-01-18 08:12:02 +0545

    add: counter program with increment, reset and decrement functionality

--- Counter/script.js
+++ Counter/script.js
@@ -6,12 +6,12 @@ let reset = document.getElementById('reset');
 number.textContent = 0;
 
 inx.addEventListener('click', () => {
-  let currentValue = parseInt(number.textContent);
+  let currentValue = parseInt(number.textContent);   // on every next click id:number vako div ko teti bela ko instance ko textContent uthauxa
   number.textContent = currentValue + 1;
 });
 
 dcx.addEventListener('click', () => {
-  currentValue = parseInt(number.textContent);
+  currentValue = parseInt(number.textContent); // same as for inx
   number.textContent = currentValue - 1;
 });
 
